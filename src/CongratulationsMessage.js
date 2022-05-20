c8c6ac14a847120bb85380771b53b2105501250b
shawnee513 <shanpan513@gmail.com>
2022-05-20 12:11:26 -0600

    can't remember

--- src/CongratulationsMessage.js
+++ src/CongratulationsMessage.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-export const CongratulationsMessage = ({numberOfClicks, threshold }) => {
-    return numberOfClicks >= threshold
-    ? <h1>Congratulations! You've reached {threshold} number of clicks</h1>
-    : null;
+export const CongratulationsMessage = ({numberOfClicks, threshold, onHide }) => {
+    return (
+        <>
+            {numberOfClicks >= threshold
+            ? <h1>Congratulations! You've reached {threshold} number of clicks</h1>
+            : null}
+            <button onClick={onHide}>Hide</button>
+        </>
+    )
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Greeting } from '../Greeting';
 
 export const HomePage = () => (
-    console.log("home page loading"),
     <>
     <h1>TheHomePage</h1>
     <Greeting name='Shawnee' numberOfMessages={10} />
