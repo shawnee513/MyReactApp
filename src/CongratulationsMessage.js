694c9c0a336e8a991c05dfc4e8c9e2c57025cca2
shawnee513 <shanpan513@gmail.com>
2022-05-20 12:23:13 -0600

    Fixed the hide button issue

--- src/CongratulationsMessage.js
+++ src/CongratulationsMessage.js
@@ -4,9 +4,11 @@ export const CongratulationsMessage = ({numberOfClicks, threshold, onHide }) => 
     return (
         <>
             {numberOfClicks >= threshold
-            ? <h1>Congratulations! You've reached {threshold} number of clicks</h1>
+            ? <>
+                <h1>Congratulations! You've reached {threshold} number of clicks</h1>
+                <button onClick={onHide}>Hide</button>
+            </>
             : null}
-            <button onClick={onHide}>Hide</button>
         </>
     )
 }
