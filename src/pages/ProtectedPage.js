00847aa26b39d10b4c17e852dd7e9fd246d7436d
shawnee513 <shanpan513@gmail.com>
2022-05-25 13:33:40 -0600

    changed redirect to doing it programmatically

--- src/pages/ProtectedPage.js
+++ src/pages/ProtectedPage.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Redirect, useHistory } from 'react-router-dom';
+
 
 export const ProtectedPage =() => {
+    const history = useHistory();
+
     const isAuthed = false;
 
-    return isAuthed
-    ? (
+    useEffect(() => {
+        if (!isAuthed) {
+            history.push('/');
+        }
+    });
+
+    return (
         <h1>Only authorized users can see this</h1>
-    ): (
-        <Redirect to="/" />
     )
 }
